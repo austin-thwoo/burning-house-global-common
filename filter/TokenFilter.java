420aed98f46367e1952ca3fff9547d7967259fc4
austin-thwoo <austin-thwoo@naver.com>
2023-03-27 15:34:42 +0900

    🎨feat:filter #2

---
+++ filter/TokenFilter.java
@@ -0,0 +1,37 @@
+package com.codingfist.burninghouseuser.globalcommon.globalcommon.filter;
+
+
+
+
+public class TokenFilter extends GenericFilterBean {
+
+
+
+
+    private final TokenProvider tokenProvider;
+
+    public TokenFilter(TokenProvider tokenProvider){
+
+        this.tokenProvider =tokenProvider;
+    }
+
+
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+
+
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+
+        if(token !=null && tokenProvider.validateToken(token)){
+
+            Authentication auth = tokenProvider.getAuthentication(token);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        }
+        chain.doFilter(request,response);
+    }
+}
